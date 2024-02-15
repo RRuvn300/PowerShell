3368a6ac640b9db3bdb4273ee956368a695acdc5
RRuvn300 <ruben.sanchez.7e7@itb.cat>
2024-02-15 10:05:03 +0100

    gola

---
+++ Scritps/Ex5.ps1
@@ -0,0 +1,22 @@
+# Script on al posar un directori, et diu si existeix o no i el llista.
+
+# Llegeix les dades
+$b = Read-Host -Prompt "Introdueix el teu fitxer o directori" 
+$a = test-path $b
+
+# Controla que si posa un espai, surt un error
+if ( $b -eq " ")
+{
+    Write-host "Posa un paràmetre vàlid"
+}
+
+# Informa si existeix o no el directori i si sí existeix, el mostra.
+elseif ( $a -eq "True")
+{
+    Write-Host "El directori" $b "sí existeix, i conté:"
+    Get-childItem $b
+} 
+else
+{
+    Write-Host "El fitxer o directori no existeix"
+}
