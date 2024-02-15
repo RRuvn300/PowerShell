f8d4797df465a3382e99bf1b3bb57ef3db5d555f
RRuvn300 <ruben.sanchez.7e7@itb.cat>
2024-02-15 09:39:50 +0100

    DIOOS!

---
+++ Scritps/Ex4.ps1
@@ -0,0 +1,11 @@
+$b = Read-Host -Prompt "Introdueix el teu fitxer o directori"
+$a = test-path $b
+
+if ( $a -eq "True")
+{
+    Write-Host "El fitxer o directori sí existeix"
+} 
+else
+{
+    Write-Host "El fitxer o directori no existeix"
+}
