e4fc8958f2b200e72b809dcc89a88b8fc59a0a16
RRuvn300 <ruben.sanchez.7e7@itb.cat>
2024-04-17 10:32:03 +0200

    HOLA

---
+++ Scritps/Export_file.ps1
@@ -0,0 +1,20 @@
+$OUPath = "OU=Usuaris,DC=RSC,DC=itb,DC=cat"
+
+$Users = Get-ADUser -Filter *  -SearchBase $OUPath -Properties *
+
+$header = "FirstName,LastName,DisplayName,Office,Email,Username"
+
+$csvFile = "Usuaris_$((Get-Date).ToString('yyyyMMdd_HHmmss')).csv"
+
+$header | Out-File -FilePath $csvFile -Encoding UTF8 -Append
+
+foreach ($User in $Users)
+{
+    $Name = $User.Name
+    $Email = $User.EmailAddress
+    $Username = $User.SamAccountName
+    $line = "$Name,$Email,$Username"
+    $line | Out-File -FilePath $csvFile -Encoding UTF8 -Append
+}
+
+Write-Host "Se generó el archivo CSV: $csvFile"
